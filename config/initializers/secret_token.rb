cfccc9b27fe5026e76546c2977743417a2e694fc
prompt-jc <jorge@prompt.cl>
2014-02-03 00:09:15 -0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = 'b3551736a5f1aa862611c08ec5e70c06096b6329dd6e5750ca30846e187ba63333c95edef0da5207b17f8a0785609b3fc179fc4d9c1268f4c93a77da79e0fc50'
